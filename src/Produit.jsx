f3788a32f1bc13b57d1a078a5e9ba02bbe9623d6
Camille Semaan <cdem@xvitae.com>
2021-02-15 13:02:15 -0500

    Début gestion du panier

--- src/Produit.jsx
+++ src/Produit.jsx
@@ -3,14 +3,20 @@ import './Produit.scss';
 
 export default function Produit(props) {
   console.log("Le paramètre props dans Produit : ", props);
+
+  function ajouterAuPanier() {
+
+  }
+
   return (
     <li className="Produit">
       <img src={'images-produits/' + props.id + '.webp'} alt={props.nom}/>
       <div className="info">
         <p className="nom">{props.nom}</p>
         <p className="prix">{props.prix} $CA</p>
+        <span></span>
       </div>
-      <BtnAjoutPanier/>
+      <BtnAjoutPanier onClick={ajouterAuPanier} />
     </li>
   );
 }
