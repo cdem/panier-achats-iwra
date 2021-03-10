33bf7991c56825bb8d639ff821e0a13d3027915e
Camille Semaan <cdem@xvitae.com>
2021-03-10 13:53:39 -0500

    Code avec React Router Dom complété.

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -5,14 +5,14 @@ import SommairePanier from './SommairePanier';
 import { Link, NavLink } from 'react-router-dom';
 
 export default function Entete(props) {
-  const [panier, setPanier] = props.etatPanier;
+  const [panier] = props.etatPanier;
   return (
     <header className="Entete">
       <div className="logo"><Link to="/">Logo</Link></div>
       <ul className="navPrincipale">
-        <li><Link to="/nos-produits">Produits</Link></li>
-        <li><Link to="/a-propos-de-nous">À propos de nous</Link></li>
-        <li><Link to="/contactez-nous">Contactez-nous</Link></li>
+        <li><NavLink activeClassName="urlActive" to="/nos-produits" >Produits</NavLink></li>
+        <li><NavLink activeClassName="urlActive" to="/a-propos-de-nous">À propos de nous</NavLink></li>
+        <li><NavLink activeClassName="urlActive" to="/contactez-nous">Contactez-nous</NavLink></li>
       </ul>
       <ul className="navUtilisateur">
         <li>
